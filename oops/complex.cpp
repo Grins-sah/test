fb9dff5bd7aa6d1f6ebb403dfff1d2352b854a61
GRINS TRIBHUWAN KUMAR SAH <163144974+Grins-sah@users.noreply.github.com>
2025-04-28 11:26:39 +0530

    Create complex.cpp

---
+++ oops/complex.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Complex {
+private :
+    int real,imag;
+public :
+    Complex()
+    {
+        real = 0;
+        imag = 0;
+    }
+    Complex(int r,int i) {
+
+        real = r;
+        imag = i;
+    }
+    void print () {
+
+        cout<<real<<"+"<<imag<<"i"<<endl;
+    }
+    Complex operator + (Complex c) {
+
+        Complex temp;
+
+        temp.real = real + c.real;
+
+        temp.imag = imag + c.imag;
+
+        return temp;
+    }
+};
+int main(){
+
+    Complex c1(5,4);
+
+    Complex c2(2,5);
+
+    Complex c3;
+    c3 = c1 + c2;  // c3 = c1.add(c2)
+
+    c3.print();//prints 7+9i
+
+    return 0;
+}
